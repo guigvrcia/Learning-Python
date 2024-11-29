5ced92fec15ddd1344cd3526d2d2ff6c62c05343
guilherme garcia <143908779+guigvrcia@users.noreply.github.com>
2024-11-29 18:31:04 -0300

    Create exercicio1.py

---
+++ Exercícios/exercicio1.py
@@ -0,0 +1,13 @@
+nome = 'Harry' # Recebe a string nome;
+sobrenome = 'Styles' # Recebe a string sobrenome;
+idade = 30 # Recebe a idade;
+ano_nascimento = 2024 - idade # Recebe o ano de nascimento;
+maior_idade = idade >= 18 # Recebe a idade e verifica se é maior de idade;
+altura_metros = 1.83 # Recebe a altura em metros;
+
+print('Nome:', nome) # Imprime o nome;
+print('Sobrenome:', sobrenome) # Imprime o sobrenome;
+print('Idade:', idade) # Imprime a idade;
+print('Ano de Nascimento:', ano_nascimento) # Imprime o ano de nascimento;
+print('É maior de idade?', maior_idade) # Imprime se é maior de idade;
+print('Altura em metros:', altura_metros) # Imprime a altura em metros;
