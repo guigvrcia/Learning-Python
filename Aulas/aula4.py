a26dd5689c39395ef7f1b579952b7a1a53ddf2f6
guilherme garcia <143908779+guigvrcia@users.noreply.github.com>
2024-11-29 13:28:34 -0300

    Create aula4.py
    Tipos de dado inteiro e float.

---
+++ Aulas/aula4.py
@@ -0,0 +1,17 @@
+# Tipos: int e float;
+
+# Numeros inteiros;
+print(11) # int
+print(-11) # int
+print(0) # int
+
+# Tipo: float;
+# Float sem sinal é considerado positivo;
+print(1.1, 10.11) 
+print(0.0, -1.5)
+
+# A classe type mostra o tipo que o Python atribuiu ao valor;
+print(type('Harry')) # String
+print(type(11))      # int
+print(type(-11))     # int
+print(type(1.1))     # float
