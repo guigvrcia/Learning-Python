8a8425f809686084a674c37ad126786c4d0d75f0
guilherme garcia <143908779+guigvrcia@users.noreply.github.com>
2024-11-29 18:31:43 -0300

    Add files via upload

---
+++ Aulas/aula7.py
@@ -0,0 +1,17 @@
+# Variáveis em Python: utilizadas para salvar algo na memória do computador;
+# Introdução a PEP8: Guia de estilo para codificação em Python;
+
+nome_completo = 'Harry Styles'
+soma_dois_mais_dois = 2 + 2
+int_um = bool('1')
+print(int_um, type(int_um)) #int
+print(nome_completo, soma_dois_mais_dois) # Harry Styles, 4
+
+# Seção usando as variáveis;
+
+nome = 'Harry'
+idade = 30
+maior_de_idade = idade >= 18
+
+print('Nome:', nome, 'Idade:', idade)
+print('É maior de idade?', maior_de_idade)
---
+++ Aulas/aula9.py
@@ -0,0 +1,29 @@
+# Introdução aos operadores aritméticos;
+
+# Adição:
+adicao = 10 + 10
+print('Adição:', adicao)
+
+# Subtração:
+subtracao = 10 - 10
+print('Subtração:', subtracao)
+
+# Multiplicação:
+multiplicacao = 10 * 10
+print('Multiplicação:', multiplicacao)
+
+# Divisão:
+divisao = 10 / 3 # float
+print('Divisão:', divisao)
+
+# Divisão inteira:
+divisao_inteira = 10 // 3 # int
+print('Divisão inteira:', divisao_inteira)
+
+# Exponenciação:
+exponenciacao = 2 ** 10
+print('Exponenciação:', exponenciacao)
+
+# Módulo:
+modulo = 55 % 2 # resto da divisão;
+print('Módulo:', modulo)
