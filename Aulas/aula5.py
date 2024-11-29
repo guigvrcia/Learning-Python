6d2e09c2f9aa505618168c6e02e2a80b40532393
guilherme garcia <143908779+guigvrcia@users.noreply.github.com>
2024-11-29 13:28:55 -0300

    Create aula5.py
    Tipo de dado booleano.

---
+++ Aulas/aula5.py
@@ -0,0 +1,10 @@
+# Tipo de dado bool (boolean);
+# Operadores para questionar: ==, questiona se um valor é igual a outro;
+# Sim = True; Não = False;
+
+print(10 == 10) # Sim => True
+print(10 == 11) # Não => False
+print(type(True)) # bool
+print(type(False)) # bool
+print(type(10 == 10)) # bool
+print(type(10 == 11)) # bool
