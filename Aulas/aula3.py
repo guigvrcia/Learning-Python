3038ebfcff33e626ee20d28609be1bc1c326d4fd
guilherme garcia <143908779+guigvrcia@users.noreply.github.com>
2024-11-29 12:56:51 -0300

    Create aula3.py
    Aula sobre o uso de strings.

---
+++ Aulas/aula3.py
@@ -0,0 +1,20 @@
+# Tipo str (String);
+# String: são textos que estão dentro de aspas simples ou duplas;
+
+print(1234)
+
+# Aspas simples;
+print('Harry Styles')
+
+# Aspas duplas;
+print("Harry Styles")
+
+# Escape
+print("Harry \"Styles")
+
+# r;
+print(r"Harry \"Styles\"")
+
+# Aspas duplas dentro de aspas simples é possível;
+print(1, 'Harry "Styles"')
+print(2, "Harry 'Styles'") # Inversão também possível;
