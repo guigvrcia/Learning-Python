9ebb2830403b3e3d5e4c553aabefd168fc4aa0d9
guilherme garcia <143908779+guigvrcia@users.noreply.github.com>
2024-11-30 01:35:26 -0300

    Add files via upload

---
+++ Aulas/aula10.py
@@ -0,0 +1,9 @@
+# Aula sobre concatenação:
+
+concatenacao = 'Harry' + ' ' + 'Styles' 
+print(concatenacao) # Imprime Harry Styles
+
+letra_a_dez = 'A' * 10
+tres_vezes_harry = 3 * 'Harry'
+print(letra_a_dez) # Imprime a letra 'A' 10 vezes.
+print(tres_vezes_harry) # Imprime Harry 3 vezes.
---
+++ Aulas/aula11.py
@@ -0,0 +1,9 @@
+# Aula sobre precedência entre os operadores aritméticos:
+
+# 1. Parênteses; (n + n)
+# 2. Potenciação; **
+# 3. Multiplicação e divisão; * / // %
+# 4. Adição e subtração. + -
+
+conta_1 = (1 + 1) ** (5 + 5)
+print(conta_1) # Imprime 1024
