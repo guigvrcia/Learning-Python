7479b56c474fa650dc5770439c58fd8cdad4a8f0
guilherme garcia <143908779+guigvrcia@users.noreply.github.com>
2024-11-30 01:35:55 -0300

    Add files via upload

---
+++ Exercícios/exercicio2.py
@@ -0,0 +1,14 @@
+# Calculo de IMC;
+
+nome = 'Harry Styles' # Nome do usuário;
+altura = 1.83 # Altura do usuário;
+peso = 70 # Peso do usuário;
+imc = ... # Placeholder para fazer o cálculo de IMC;
+
+# Cálculo do IMC;
+
+imc = peso / (altura ** 2) # Fórmula para calcular o IMC;
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu IMC é:')
+print(imc)
